1ca4bda91002101b2f80e9389551da0089e1631a
joaolavelino <joaolavelino@gmail.com>
2022-05-25 10:27:57 -0300

    1 - Sanity Setup

---
+++ recipe-studio/schemas/chef.js
@@ -0,0 +1,33 @@
+export default {
+  name: "chef",
+  //the title is the name of the schema this object belongs
+  title: "Chef",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Chef's name",
+      type: "string",
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true, // <-- Defaults to false
+      },
+    },
+    {
+      name: "bio",
+      title: "Bio",
+      type: "array",
+      of: [
+        {
+          title: "Block",
+          type: "block",
+          styles: [{ title: "Normal", value: "normal" }],
+        },
+      ],
+    },
+  ],
+};
